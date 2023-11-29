5ed2ecef5adc99ad04cbe2719e2c1bdd96a27421
Melvin Vasquez <mvasq95@hotmail.com>
2023-11-29 13:11:27 -0500

    commenting out TS code, starting with enums and type aliases

--- src/script.ts
+++ src/script.ts
@@ -1,120 +1,148 @@
 const windowDOM = document.getElementById('window') as HTMLElement;
 
-//using a union type to make currentValue a number or string
-let currentValue: number | string = 0;
-let previousValue = 0;
-let currentOperator = "none";
-
-windowDOM.innerHTML = currentValue.toString();
-
-//Helper Function
-function solveIt(operator: string){
-
-    if(typeof currentValue === "number"){
-        if (operator === "divide"){
-            currentValue = previousValue / currentValue;
-        }
-        if (operator === "multiply"){
-            currentValue = previousValue * currentValue
-        }
-        if (operator === "subtract"){
-            currentValue = previousValue - currentValue
-        }
-        if (operator === "add"){
-            currentValue = previousValue + currentValue
-        }
-        //display decimals to the thousandth(0.001)
-        currentValue = Math.round(currentValue * 1000) / 1000
-        //if number too big or too small to display, show ERROR
-        if(currentValue > 9999999999 || currentValue < 0.00000001){
-            currentValue="ERROR";
-        }
-        //reset values to proceed with more calculations
-        currentOperator="none";
-        previousValue=0;
-    }
-
+enum Digit {
+    Zero = '0',
+    One = '1',
+    Two = '2',
+    Three = '3',
+    Four = '4',
+    Five = '5',
+    Six = '6',
+    Seven = '7',
+    Eight = '8',
+    Nine = '9',
+    Decimal = '.',
 }
 
+enum Operator{
+    Add = 'add',
+    Subtract = 'subtract',
+    Multiply = 'multiply',
+    Divide = 'divide',
+    None = 'none'
+}
 
-window.addEventListener('click', function(event){
-    console.log("test");
+type DigitTuple = [Digit, ...Digit[]] & { length: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 };
+
+let currentInputs: DigitTuple = [Digit.Zero]; 
+let previousInputs: DigitTuple = [Digit.Zero]; 
+let currentOperator: Operator.None;
+
+console.log(currentInputs);
+
+// windowDOM.innerHTML = currentInputs.toString();
+
+// //Helper Function
+// function solveIt(operator: string){
+
+//     if(typeof currentInputs === "number"){
+//         if (operator === "divide"){
+//             currentInputs = previousInputs / currentInputs;
+//         }
+//         if (operator === "multiply"){
+//             currentInputs = previousInputs * currentInputs
+//         }
+//         if (operator === "subtract"){
+//             currentInputs = previousInputs - currentInputs
+//         }
+//         if (operator === "add"){
+//             currentInputs = previousInputs + currentInputs
+//         }
+//         //display decimals to the thousandth(0.001)
+//         currentInputs = Math.round(currentInputs * 1000) / 1000
+//         //if number too big or too small to display, show ERROR
+//         if(currentInputs > 9999999999 || currentInputs < 0.00000001){
+//             currentInputs="ERROR";
+//         }
+//         //reset values to proceed with more calculations
+//         currentOperator="none";
+//         previousInputs=0;
+//     }
+
+// }
 
 
-    //using type asserction to inform `event.target` as `Element` to then use appropriate methods.
-    const targetElement = event.target as Element;
-    const targetValue = targetElement.getAttribute('value');
+// window.addEventListener('click', function(event){
+
+//     //using type asserction to inform `event.target` as `Element` to then use appropriate methods.
+//     const targetElement = event.target as Element;
+//     const targetValue = targetElement.getAttribute('value');
     
-    if (targetValue !== null) {
-        //IF reset button pressed
-        if(targetElement.matches('#clear')){
-            currentValue = 0;
-            previousValue  = 0;
-            currentOperator = "none";
-        }
-        //IF a number is pressed
-        if(targetElement.matches('.number')){
-            //IF writing first value
-            if(currentOperator == "none" && previousValue == 0 && typeof currentValue == "number"){
-                //Display no more than 10 digits on the screen
-                if(currentValue < 1000000000){
-                    //IF value is zero, Prevent Leading Zeroes
-                    if(currentValue === 0){
-                        currentValue = targetValue.toString();
-                    } 
-                    //ELSE concat the number
-                    if(typeof currentValue === "string" && currentValue != "Error"){
-                        currentValue = currentValue.concat(targetValue.toString());
-                    }
-                }
-            }
-            //IF an operator has been selected
-            if(currentOperator !== "none"){
-                //IF first digit of new value, save initial value, then display new value
-                if(previousValue == 0 && typeof currentValue == "string" && currentValue != "Error"){
-                    previousValue = parseInt(currentValue);
-                    currentValue = targetValue.toString();
-                }
-                //ELSE concat digit to new value
-                if(typeof currentValue === "string" && currentValue != "Error"){
-                    currentValue = currentValue.concat(targetValue.toString());
-                }
-            }
+//     if (targetValue !== null) {
+//         //IF reset button pressed
+//         if(targetValue === "clear"){
+//             currentInputs = 0;
+//             previousInputs  = 0;
+//             currentOperator = "none";
+//         }
+//         //IF a number is pressed
+//         if(targetElement.matches('.number')){
+//             //IF writing first value
+            
+//             if(currentOperator == "none" && typeof currentInputs == "number"){
+
+//                 //Display no more than 10 digits on the screen
+//                 if(currentInputs < 1000000000){
+//                     //IF value is zero, Prevent Leading Zeroes
+//                     if(currentInputs === 0){
+//                         currentInputs = targetValue;
+//                     }
+//                     else{
+//                         console.log("this is the type of targetvalue" + typeof targetValue)
+//                     }
+//                     //ELSE concat the number
+//                     // else(typeof currentInputs === "string" && currentInputs != "Error"){
+//                     //     currentInputs = currentInputs.concat(targetValue.toString());
+//                     // }
+//                 }
+//             }
+//             //IF an operator has been selected
+//             if(currentOperator !== "none"){
+//                 //IF first digit of new value, save initial value, then display new value
+//                 if(previousInputs == 0 && typeof currentInputs == "string" && currentInputs != "Error"){
+//                     previousInputs = parseInt(currentInputs);
+//                     currentInputs = targetValue.toString();
+//                 }
+//                 //ELSE concat digit to new value
+//                 if(typeof currentInputs === "string" && currentInputs != "Error"){
+//                     currentInputs = currentInputs.concat(targetValue.toString());
+//                 }
+//             }
     
-        }
-        //IF the decimal is pressed
-        if(targetElement.matches('#decimal')){
-            //prevent additional decimals
-            if(typeof currentValue == "string" && !currentValue.includes(".")){
-                currentValue = currentValue.concat(targetValue.toString());
-            }
-        }
-        //IF operator selected
-        if(targetElement.matches('.operator')){
-            //IF FIRST TIME USING OPERATOR
-            if(currentOperator === "none"){
-                currentOperator = targetValue //define operation
-                return event.preventDefault() //prevent next step due to defined operation
-            }
-            //IF NOT FIRST TIME USING OPERATOR
-            if(currentOperator !== "none"){
-                solveIt(currentOperator) //execute previous operator
-                currentOperator = targetValue //queue operator for next calculation
-            }
+//         }
+        // //IF the decimal is pressed
+        // if(targetElement.matches('#decimal')){
+        //     //prevent additional decimals
+        //     if(typeof currentInputs == "string" && !currentInputs.includes(".")){
+        //         currentInputs = currentInputs.concat(targetValue.toString());
+        //     }
+        // }
+        // //IF operator selected
+        // if(targetElement.matches('.operator')){
+        //     //IF FIRST TIME USING OPERATOR
+        //     if(currentOperator === "none"){
+        //         currentOperator = targetValue //define operation
+        //         return event.preventDefault() //prevent next step due to defined operation
+        //     }
+        //     //IF NOT FIRST TIME USING OPERATOR
+        //     if(currentOperator !== "none"){
+        //         solveIt(currentOperator) //execute previous operator
+        //         currentOperator = targetValue //queue operator for next calculation
+        //     }
     
-        }
-        //IF solve button pressed 
-        if(targetElement.matches('#equals')){
-            solveIt(currentOperator);
-        }
-    }
+        // }
+        // //IF solve button pressed 
+        // if(targetElement.matches('#equals')){
+        //     solveIt(currentOperator);
+        // }
+    // }
 
 
 
-    event.preventDefault() //prevent window from reloading on button press
-    windowDOM.innerHTML = currentValue.toString(); //write to DOM value/calculation
+    // event.preventDefault() //prevent window from reloading on button press
+    // windowDOM.innerHTML = currentInputs.toString(); //write to DOM value/calculation
 
-})
+// })
 
 
 
